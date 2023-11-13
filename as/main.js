baa43f7cc172b0d7243ad1d3d591ed03918603c8
rodrigorioss <rori5590@colorado.edu>
2023-11-12 18:40:06 -0700

    j

--- as/main.js
+++ as/main.js
@@ -3,10 +3,10 @@ const thumbBar = document.querySelector('.thumb-bar');
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
-// Declaring the array of image filenames
+
 const imageFilenames = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 
-// Declaring the alternative text for each image file
+
 const altTexts = {
   'pic1.jpg': 'Closeup of a blue human eye',
   'pic2.jpg': 'Image description for pic2',
@@ -15,21 +15,21 @@ const altTexts = {
   'pic5.jpg': 'Image description for pic5'
 };
 
-// Looping through images
+
 imageFilenames.forEach(filename => {
   const newImage = document.createElement('img');
   newImage.setAttribute('src', 'images/' + filename);
   newImage.setAttribute('alt', altTexts[filename]);
   thumbBar.appendChild(newImage);
   
-  // Adding a click event listener to each thumbnail image
+  
   newImage.addEventListener('click', e => {
     displayedImage.src = e.target.src;
     displayedImage.alt = e.target.alt;
   });
 });
 
-// Wiring up the Darken/Lighten button
+
 btn.addEventListener('click', () => {
   const className = btn.getAttribute('class');
   if (className === 'dark') {
