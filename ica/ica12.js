db74629b4d18901a091edd7d95b38720f156eeaa
rodrigorioss <rori5590@colorado.edu>
2023-11-15 16:43:24 -0700

    j

--- ica/ica12.js
+++ ica/ica12.js
@@ -2,6 +2,10 @@ const newQuoteButton = document.querySelector('#js-new-quote');
 
 newQuoteButton.addEventListener('click', getQuote);
 
+const showAnswerButton = document.querySelector('#js-tweet');
+
+showAnswerButton.addEventListener('click', displayAnswer);
+
 function getQuote() {
     console.log("Button clicked!"); // Check the button click
 
@@ -11,6 +15,8 @@ function getQuote() {
         .then(response => response.json())
         .then(data => {
             console.log(data);
+
+            currentTriviaData = data;
 
             displayQuote(data.question);
         })
@@ -24,5 +30,16 @@ function displayQuote(quote) {
     const quoteTextElement = document.getElementById('js-quote-text');
     quoteTextElement.textContent = quote;
 }
+
+function displayAnswer() {
+    if (currentTriviaData && currentTriviaData.answer) {
+        const answerTextElement = document.getElementById('js-answer-text');
+        answerTextElement.textContent = `Answer: ${currentTriviaData.answer}`;
+    } else {
+        console.log("No answer available");
+    }
+}
+
+let currentTriviaData = null;
 
 document.addEventListener('DOMContentLoaded', getQuote);
