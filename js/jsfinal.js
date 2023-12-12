109515c4d509f586ee07e09816a2eb70b4d7a01d
rodrigorioss <rori5590@colorado.edu>
2023-12-12 08:32:03 -0700

    yee

--- js/jsfinal.js
+++ js/jsfinal.js
@@ -2,25 +2,25 @@ document.addEventListener("DOMContentLoaded", function () {
     const digitContainers = document.querySelectorAll('.digit-container');
     const resetButton = document.getElementById('reset');
     const submitButton = document.getElementById('submit');
+    const displayBox = document.getElementById('display-box');
     let phoneNumber = '';
 
     digitContainers.forEach(container => {
         container.addEventListener('click', function () {
             const digit = this.querySelector('.digit').getAttribute('data-digit');
             phoneNumber += digit;
-            // You can update an input field or display the selected digits as needed
-            console.log('Selected digits: ' + phoneNumber);
+            displayBox.textContent = phoneNumber;
         });
     });
 
     resetButton.addEventListener('click', function () {
         phoneNumber = '';
-        // You can reset any input field or display here
+        displayBox.textContent = '';
         console.log('Phone number reset.');
     });
 
     submitButton.addEventListener('click', function () {
-        // You can perform actions with the collected phone number on submit
         console.log('Submitting phone number: ' + phoneNumber);
+        // Add additional logic for submitting the phone number, if needed
     });
 });
